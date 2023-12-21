d04bdcf253779266fd89c4ba92d26ccb079e38b0
dhruv singh <dhruvs437@MacBook-Air-3.local>
2023-12-21 23:01:22 +0530

    deploying 2

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+require('dotenv').config();
 const port = process.env.port||5000;
 const mongoose = require("mongoose");
 const {mongoUrl}=require("./keys")
