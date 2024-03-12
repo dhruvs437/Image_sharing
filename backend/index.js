d7e4ee59860a5b21285fad375a9d0e5b857209f5
Dhruv Singh <dhruvsinghs437@gmail.com>
2024-03-12 12:41:22 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 require('dotenv').config();
-const port = process.env.port||5000;
+const port = process.env.port||6000;
 const mongoose = require("mongoose");
 const {mongoUrl}=require("./keys")
 //to eliminate cors policy which occurs beacuse of different domain of fronted and backend
