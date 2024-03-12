c51c64079abe3f557d0181f45687c10669eaaa18
Dhruv Singh <dhruvsinghs437@gmail.com>
2024-03-12 23:30:59 +0530

    messenger comment

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -29,9 +29,9 @@ export default function Navbar({ login }) {
           <Link to={"/createPost"}>
             <li>Create Post</li>
           </Link>
-          <Link to={"/messenger"}>
+          {/* <Link to={"/messenger"}>
             <li>Messenger</li>
-          </Link>
+          </Link> */}
           <Link style={{ marginLeft: "20px" }} to={"/followingpost"}>
             My Following
           </Link>
